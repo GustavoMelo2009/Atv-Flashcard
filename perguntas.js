b8864691bec1a1dd453febf7e403543bc93a1dc5
GustavoMelo2009 <gustavo.lazzari.melo@escola.pr.gov.br>
2025-07-04 13:35:20 -0300

    Foi adicionado mais cartões/Flashcards

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,62 @@ criaCartao(
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
+
+criarCartao (
+    'Português',
+    'O que é uma proparoxítona?',
+    'Português',
+    'Uma proparoxítona é uma palavra cuja sílaba tônica (aquela pronunciada com mais intensidade) é a antepenúltima.'
+
+)
+criarCartao (
+     'Matemática',
+    'Qual é a fórmula do Teorema de Pitágoras?',
+    'a²=b²+c²'
+
+)
+criarCartao (
+     'História',
+    'Qual foi o último czar que a Rússia teve  antes da Revolução Russa?',
+    'Czar Nicolau II'
+
+)
+criarCartao (
+    'Geografia',
+    'Qual é a cidade mais populosa do mundo?',
+    'Tokyo'
+)
+criarCartao (
+     'Filosofia',
+    'Quem disse: "Penso, logo existo" ?',
+    'René Descartes'
+
+)
+criarCartao (
+     'Sociologia',
+    'O que é cultura para a Sociologia?',
+    'Um conjunto de valores, normas e práticas sociais compartilhadas'
+
+)
+criarCartao (
+    'Física',
+    'Qual é a fórmula da velocidade média?',
+    'Vm = ΔS / Δt'
+
+)
+criarCartao (
+     'Química',
+    ' O que é uma reação de neutralização?',
+    ' É a reação entre um ácido e uma base, formando sal e água'
+)
+criarCartao (
+    'Biologia',
+    'Onde ocorre a fotossíntese?',
+    ' Nos cloroplastos das células vegetais'
+)
+criarCartao (
+     'Inglês',
+    'Qual o passado de "go" em inglês?',
+    'Went'
+
+)
